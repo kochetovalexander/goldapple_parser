6afda6f5f43398f88b47ebea082d4412a86bc2c4
Alexander Kochetov <alexk@imedia.ru>
2024-04-01 22:54:53 +0300

    updates

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from selenium import webdriver as wd
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
-URL = "https://goldapple.ru/parfjumerija"
+URL = "https://goldapple.ru/uhod"
 CSV_PATH = "products.csv"
 
 opts = wd.FirefoxOptions()
@@ -21,8 +21,6 @@ def main() -> None:
     firefox_driver = wd.Firefox(options=opts)
 
     print("Загрузка данных...")
-    product_urls = get_all_products_urls(start_page=1, end_page=414, driver=firefox_driver)
-    print("Готово! Получено {} товаров.".format(len(product_urls)))
 
     # product_urls = ["https://goldapple.ru/26830400003-dreaming-with-ghosts",
     #                 "https://goldapple.ru/7430500002-eau-fraiche",
@@ -30,37 +28,53 @@ def main() -> None:
     #                 ]
 
     print("Обработка данных...")
-    items_list: list[dict] = []
-    item_number = 0
-    for _url in product_urls:
-        make_selenium_get_request(_url, firefox_driver)
 
-        time.sleep(1.5)
+    for page in range(1, 1921):
+        product_urls: list = []
 
-        item_number: int = item_number + 1
-        item_name: str = get_item_name(driver=firefox_driver)
-        item_price: str = get_item_price(driver=firefox_driver)
-        item_rating: float | str = get_item_rating(driver=firefox_driver)
-        item_description: str = get_item_description(driver=firefox_driver)
-        item_instructions, item_country = manipulate_menu(driver=firefox_driver)
+        make_selenium_get_request(URL, firefox_driver, page)
+        tablet_items_class = get_items_class(firefox_driver)
+        product_urls = get_items_urls_on_page(tablet_items_class)
+        print(f"Страница {page} обработана.")
+        time.sleep(2)
 
-        items_list.append(create_item_dict(
-            number=item_number,
-            link=_url,
-            name=item_name,
-            price=item_price,
-            rating=item_rating,
-            description=item_description,
-            instructions=item_instructions,
-            country=item_country
-        ))
+        items_list: list[dict] = []
+        item_number = 0
+        for _url in product_urls:
+            make_selenium_get_request(_url, firefox_driver)
 
-        print("Товар {} создан!".format(item_name))
+            time.sleep(1.5)
 
-    print("Все товары были созданы. Процесс сохранения...")
-    items_dataframe = get_dataframe(items_list)
-    save_to_csv(items_dataframe, CSV_PATH)
-    print("Все товары сохранены в {}".format(CSV_PATH))
+            item_number: int = item_number + 1
+            item_name: str = get_item_name(driver=firefox_driver)
+            item_image: str = get_item_image(driver=firefox_driver)
+            item_price: str = get_item_price(driver=firefox_driver)
+            item_rating: float | str = get_item_rating(driver=firefox_driver)
+            item_description: str = get_item_description(driver=firefox_driver)
+            item_instructions, item_country, item_composition = manipulate_menu(driver=firefox_driver)
+
+            items_list.append({
+                "number": item_number,
+                "link": _url,
+                "name": item_name,
+                "image": item_image,
+                "price": item_price,
+                "rating": item_rating,
+                "description": item_description,
+                "composition": item_composition,
+                "instructions": item_instructions,
+                "country": item_country
+            })
+
+            print("Товар {} создан!".format(item_name))
+
+        print("Все товары были созданы. Процесс сохранения...")
+        items_dataframe = get_dataframe(items_list)
+
+        with open(CSV_PATH, 'a') as f:
+            items_dataframe.to_csv(f, header = (page == 1))
+
+        print("Все товары сохранены в {}".format(CSV_PATH))
 
     firefox_driver.close()
 
@@ -73,11 +87,14 @@ def make_selenium_get_request(url: str, driver, page: int | None = None) -> None
     request_url = url
     if page:
         request_url += f"?p={page}"
-    driver.get(request_url)
+    try:
+        driver.get(request_url)
+    except:
+        print()
 
 
 def get_items_class(driver):
-    children = driver.find_elements(By.CLASS_NAME, "Wqob-")
+    children = driver.find_elements(By.CLASS_NAME, "MHHxW")
     print(f"Это внутри: {children}")
     return children
 
@@ -128,11 +145,22 @@ def get_item_price(driver: wd):
         return "Not available"
 
 
+def get_item_image(driver: wd):
+    """Функция осуществляет поиск поле <Цена> и парсит соответствующее полю значение."""
+    try:
+        p_item_image = driver.find_element(
+            By.XPATH, '//*[@id="__layout"]/div/main/article/div[1]/div[2]/div[2]/div[1]/div/div/div/div[1]/picture/source[1]')
+        image = p_item_image.get_attribute("srcset")
+        return image
+    except NoSuchElementException:
+        return "Not available"
+
+
 def get_item_description(driver: wd):
     """Функция осуществляет поиск поле <Описание> и парсит соответствующее полю значение."""
     try:
         p_item_description = driver.find_element(
-            By.CLASS_NAME, r'G5-4J')
+            By.CLASS_NAME, r'IBqyX')
         return p_item_description.text.replace("\n", "").strip()
     except NoSuchElementException:
         return "Not available"
@@ -153,6 +181,7 @@ def manipulate_menu(driver: wd):
     """Функция осуществляет поиск полей <О бренде> и <Применение> и парсит соответствующее полю значение."""
     p_item_instructions = "Not available"
     p_item_country = "Not available"
+    p_item_composition = "Not available"
 
     for i in range(1, 5):
         try:
@@ -164,19 +193,24 @@ def manipulate_menu(driver: wd):
 
             if menu_item.text.strip() == "ПРИМЕНЕНИЕ":
                 p_item_instructions = driver.find_element(
-                    By.CLASS_NAME, r'G5-4J').text.replace("\n", "").strip()
+                    By.CLASS_NAME, r'IBqyX').text.replace("\n", "").strip()
+            elif menu_item.text.strip() == "СОСТАВ":
+                p_item_composition = driver.find_element(
+                    By.CLASS_NAME, r'IBqyX').text.strip()
             elif menu_item.text.strip() == "О БРЕНДЕ":
                 p_item_country = driver.find_element(
-                    By.CLASS_NAME, r'G4xy5').text.strip()
+                    By.CLASS_NAME, r'bFC3b').text.strip()
 
         except NoSuchElementException:
             continue
+        except:
+            continue
 
-    return p_item_instructions, p_item_country
+    return p_item_instructions, p_item_country, p_item_composition
 
 
 def create_item_dict(
-        number: int, link: str, name: str, price: float = 0, rating: float | str = 0, description: str = "",
+        number: int, link: str, name: str, price: float = 0, rating: float | str = 0, description: str = "", composition: str = "",
         instructions: str = "", country: str = ""
 ) -> dict[str, str | float]:
     """Функция создает словарь, представляющий отдельный товар с указанными характеристиками."""
